f03033cc8bb6df8b3b96b84f6cc4a0cc41f0b310
pratikkuyate96 <62277306+pratikkuyate96@users.noreply.github.com>
2020-05-13 16:41:33 +0530

    ADDED: To calculated Minimum Fare

--- src/test/java/CabServices/InvoiceServiceTest.java
+++ src/test/java/CabServices/InvoiceServiceTest.java
@@ -4,13 +4,21 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class InvoiceServiceTest {
+    InvoiceService invoiceService = null;
 
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
-        InvoiceService invoiceService = new InvoiceService();
         double distance = 2.0;
         int time = 5;
         double fare = invoiceService.calculateFare(distance, time);
         Assert.assertEquals(25, fare, 0.0);
+    }
+
+    @Test
+    public void givenLessDistanceAndTime_ShouldReturnMinFare() {
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceService.calculateFare(distance, time);
+        Assert.assertEquals(5, fare, 0.0);
     }
 }
