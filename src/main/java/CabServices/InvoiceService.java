3fe70daa2f377dd768295ebfce5d17ca3e4b3451
pratikkuyate96 <62277306+pratikkuyate96@users.noreply.github.com>
2020-05-13 16:13:23 +0530

    ADDED: To calculated total fare

---
+++ src/main/java/CabServices/InvoiceService.java
@@ -0,0 +1,10 @@
+package CabServices;
+
+public class InvoiceService {
+    private static final int COST_PER_MINUTE =1;
+    private static final int MINIMUM_COST_PER_KILOMETER =10;
+
+    public double calculateFare(double distance, int time){
+        return distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_MINUTE;
+    }
+}
---
+++ src/test/java/CabServices/InvoiceServiceTest.java
@@ -0,0 +1,16 @@
+package CabServices;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class InvoiceServiceTest {
+
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+        InvoiceService invoiceService = new InvoiceService();
+        double distance = 2.0;
+        int time = 5;
+        double fare = invoiceService.calculateFare(distance, time);
+        Assert.assertEquals(25, fare, 0.0);
+    }
+}
